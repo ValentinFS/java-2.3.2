a84921db01874ab753b8925d6b8262e137cdc561
Valentin Fadeev <valentinfs@yandex.ru>
2021-08-24 00:00:07 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,8 @@ public class Main {
     long amount = 1000_60;
     boolean registered = true;
 
-    service.calculate(amount, true);
-
+    long actual = service.calculate(amount, true);
+    System.out.println(actual);
   }
 }
 
